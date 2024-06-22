d475161d63a501edcc6d0cfa3087c2c737c3842f
Marco Murawski <git@murasko.de>
2024-06-22 22:42:02 +0000

    feat: cleanup and password changed

--- src/models/user_models.py
+++ src/models/user_models.py
@@ -13,11 +13,8 @@ class UserInDB(User):
 
 
 fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-        "disabled": False,
+    "admin": {
+        "username": "admin",
+        "hashed_password": "$2b$12$CDVEKy.pcnJ.YN7kTsSFlOQBimLD4eydKqpikLwx7tD9d6u5LM0ua",
     }
 }
